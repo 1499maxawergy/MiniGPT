585688e65bd16f4ec12e61f9562bc89d970c2800
Maxawergy <maxawergy@yandex.ru>
2023-06-03 20:54:46 +0300

    Version 1.3 - Reworked saving of user settings - Added saving pair of request/response in SQLite Database - Fixed bug of enter spamming - Added setting of clearing prompt after sending request - Added setting of number pairs request/response in Database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,9 @@ module ru.maxawergy.desktop {
     requires javafx.fxml;
     requires com.google.gson;
     requires java.net.http;
+    requires java.prefs;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens ru.maxawergy.desktop to javafx.fxml;
--- src/main/java/ru/maxawergy/desktop/MainUI.java
+++ src/main/java/ru/maxawergy/desktop/MainUI.java
@@ -9,20 +9,47 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.*;
-import java.util.Optional;
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.prefs.Preferences;
 
 public class MainUI extends Application {
-
-    private static final String FILE_PATH = "token.txt";
+    // Локальное значение токена, которое мы берем из реестра
     private static String TOKEN;
+    // Локальное значение очистки поля запроса после его отправки
+    private boolean CLEAR;
+    // Локальное значение количества сохраняемых запросов в истории
+    private int MAX_HISTORY = 10;
+    // Все настройки пользователя храним в реестре приложения в ветке prefmain
+    private final Preferences userPreferences = Preferences.userRoot().node("prefmain");
+    // Подключение к базе данных SQLite
+    private static Connection connection;
+    // Путь к папке пользователя
+    private static String locationUserHome = System.getProperty("user.home");
+    // Путь к файлу БД
+    private static String locationDB = locationUserHome + "/.minigpt/history.db";
+
+    // Объект класса запроса по API
 
     public GPTRequest gptRequest = new GPTRequest();
+    // Текстовое поле ввода запроса в UI
     private TextField prompt;
-    private Button tokenButton;
+    // Кнопка замены настроек в UI
+    private Button settingButton;
+    // Кнопка просмотра/очистки истории в UI
+    private Button historyButton;
+    // Кнопка отправки запроса в UI
     private Button sendButton;
+    // Поле вывода ответа на запрос
     private TextArea resultTextArea;
 
 
@@ -30,25 +57,79 @@ public class MainUI extends Application {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        // Создаем корневой контейнер VBox
+    private void createDBFile() throws IOException {
+        File miniGptDir = new File(locationUserHome + "/.minigpt");
+        if (!miniGptDir.exists()) {
+            miniGptDir.mkdir();
+        }
+        File dbFile = new File(locationUserHome + "/.minigpt/history.db");
+        if (!dbFile.exists()){
+            dbFile.createNewFile();
+        }
+    }
 
+    private static void checkDrivers() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            DriverManager.registerDriver(new org.sqlite.JDBC());
+        } catch (ClassNotFoundException | SQLException classNotFoundException) {
+            Logger.getAnonymousLogger().log(Level.SEVERE, LocalDateTime.now() + ": Could not start SQLite Drivers");
+        }
+    }
+
+    private static Connection connect(String location) {
+        String dbPrefix = "jdbc:sqlite:";
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(dbPrefix + location);
+            connection.setAutoCommit(true);
+        } catch (SQLException exception) {
+            Logger.getAnonymousLogger().log(Level.SEVERE,
+                    LocalDateTime.now() + ": Could not connect to SQLite DB at " +
+                            location);
+            return null;
+        }
+        return connection;
+    }
+
+    private static void createTable() {
+        String query = "CREATE TABLE IF NOT EXISTS history (request TEXT, response TEXT)";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println("Error creating table history");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Проверка драйверов SQLite
+        checkDrivers();
+        // Создание файла базы данных
+        createDBFile();
+        // Подключение к БД
+        connection = connect(locationDB);
+        // Проверка наличия таблицы history. Если ее нет, то создание таблицы history
+        createTable();
         // Проверка наличия сохраненного значения
-        String savedValue = loadValueFromFile();
-        if (savedValue != null) {
-            TOKEN = savedValue;
+        if (userPreferences.get("token", null) != null
+                && userPreferences.get("clear", null) != null
+                && userPreferences.get("history", null) != null) {
+            TOKEN = userPreferences.get("token", null);
+            CLEAR = userPreferences.getBoolean("clear", true);
+            MAX_HISTORY = userPreferences.getInt("history", 10);
         } else {
             // Если значение не было сохранено, показываем окно с вводом значения
             showSettingsDialog();
         }
-
+        // Создаем корневой контейнер VBox
         VBox root = new VBox();
         root.setPadding(new Insets(10));
         root.setSpacing(10);
         root.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        // Создаем текстовое поле 2
+        // Создаем текстовое поле тела запроса
         prompt = new TextField();
         prompt.setStyle("-fx-background-color: #333333; -fx-text-fill: white;");
         prompt.setPromptText("Prompt");
@@ -59,9 +140,20 @@ public class MainUI extends Application {
         sendButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white;");
 
         // Кнопка замены токена
-        tokenButton = new Button("Token");
-        tokenButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white;");
-        tokenButton.setOnAction(event -> showSettingsDialog());
+        settingButton = new Button("Settings");
+        settingButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white;");
+        settingButton.setOnAction(event -> showSettingsDialog());
+
+        // Кнопка просмотра/очистки истории
+        historyButton = new Button("History");
+        historyButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white;");
+        historyButton.setOnAction(event -> {
+            try {
+                showHistoryDialog();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         Button aboutButton = new Button("About");
         aboutButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white;");
@@ -69,7 +161,7 @@ public class MainUI extends Application {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("About");
             alert.setHeaderText(null);
-            alert.setContentText("MiniGPT\n\nAuthor: 1499maxawergy\nVersion: 1.2\nGithub: https://github.com/1499maxawergy");
+            alert.setContentText("MiniGPT\n\nAuthor: 1499maxawergy\nVersion: 1.3\nGithub: https://github.com/1499maxawergy");
             DialogPane dialogPane = alert.getDialogPane();
             dialogPane.getStylesheets().add("dialog.css");
             Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
@@ -78,7 +170,7 @@ public class MainUI extends Application {
             alert.showAndWait();
         });
 
-        HBox hBox = new HBox(sendButton, tokenButton, aboutButton);
+        HBox hBox = new HBox(sendButton, settingButton, historyButton, aboutButton);
         hBox.setSpacing(10);
         root.getChildren().add(hBox);
 
@@ -101,7 +193,7 @@ public class MainUI extends Application {
             startRequestFromUI();
         });
         prompt.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ENTER) {
+            if (event.getCode() == KeyCode.ENTER && !sendButton.isDisabled()) {
                 startRequestFromUI();
             }
         });
@@ -111,9 +203,9 @@ public class MainUI extends Application {
 
         // Задаем заголовок окна
         primaryStage.setTitle("MiniChatGPT by 1499maxawergy");
-
         // Устанавливаем сцену на окно
         primaryStage.setScene(scene);
+        // Устанавливаем иконку приложения на сцену
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
         // Отображаем окно
         primaryStage.show();
@@ -122,18 +214,27 @@ public class MainUI extends Application {
     private void startRequestFromUI() {
         // Отключаем кнопку на время запроса
         sendButton.setDisable(true);
-        tokenButton.setDisable(true);
+        settingButton.setDisable(true);
+        historyButton.setDisable(true);
 
         // Получаем текст из текстовых полей
         String promptString = prompt.getText();
 
         if (promptString.isBlank()) {
             resultTextArea.setText("Prompt area is blank. Write prompt and try again");
+            sendButton.setDisable(false); // Включаем кнопку после получения ошибки
+            settingButton.setDisable(false);
+            historyButton.setDisable(false);
             return;
         }
 
-        prompt.setText("");
-        resultTextArea.setText("Request is compiling. Wait...");
+        String request = prompt.getText();
+
+        if (CLEAR) {
+            prompt.setText("");
+        }
+
+        resultTextArea.setText("Waiting for response from ChatGPT...");
 
         Task<Void> task = new Task<>() {
             @Override
@@ -142,6 +243,8 @@ public class MainUI extends Application {
                 String result = getResponse(promptString);
                 // Выводим результат в текстовое поле
                 resultTextArea.setText(result);
+                // Записываем запрос в БД
+                addHistoryItem(request, result);
                 return null;
             }
         };
@@ -150,13 +253,15 @@ public class MainUI extends Application {
         task.setOnSucceeded(e -> {
             // Здесь можно выполнить действия после успешного завершения запроса
             sendButton.setDisable(false); // Включаем кнопку после выполнения запроса
-            tokenButton.setDisable(false);
+            settingButton.setDisable(false);
+            historyButton.setDisable(false);
         });
 
         task.setOnFailed(e -> {
             // Здесь можно выполнить действия при ошибке выполнения запроса
             sendButton.setDisable(false); // Включаем кнопку после выполнения запроса
-            tokenButton.setDisable(false);
+            settingButton.setDisable(false);
+            historyButton.setDisable(false);
         });
 
         // Запускаем задачу в новом фоновом потоке
@@ -169,43 +274,145 @@ public class MainUI extends Application {
         return gptRequest.getCompletion(TOKEN, prompt);
     }
 
-    private void showSettingsDialog() {
-        // Создание диалогового окна с полем ввода
-        TextInputDialog dialog = new TextInputDialog(TOKEN);
-        dialog.setTitle("Token input");
-        dialog.setHeaderText("Enter your ChatGPT API token");
+    private void showHistoryDialog() throws SQLException {
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setTitle("History");
 
-        // Установка стилей
-        DialogPane dialogPane = dialog.getDialogPane();
-        dialogPane.getStylesheets().add("dialog.css");
-        Stage stage = (Stage) dialogPane.getScene().getWindow();
-        stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon.png")));
+        TextArea historyArea = new TextArea();
+        historyArea.setEditable(false);
+        historyArea.setWrapText(true);
+        historyArea.setStyle("-fx-control-inner-background: #444444; -fx-text-fill: white; -fx-text-alignment: left");
 
-        // Отображение диалогового окна и ожидание результата
-        Optional<String> result = dialog.showAndWait();
 
-        // Обработка введенного значения
-        result.ifPresent(this::saveValueToFile);
-    }
+        stage.heightProperty().addListener((observable, oldValue, newValue) -> {
+            double sceneHeight = (double) newValue;
+            double textAreaHeight = sceneHeight - 100; // Высота TextArea будет на 100 пикселей меньше высоты сцены
+            historyArea.setPrefHeight(textAreaHeight);
+        });
 
-    private void saveValueToFile(String value) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
-            writer.write(value);
-            TOKEN = value;
-        } catch (IOException e) {
-            e.printStackTrace();
+        StringBuilder historyRequested = new StringBuilder();
+        List<String []> allHistory = getAllHistory();
+        for (String[] reqAndResp : allHistory) {
+            historyRequested.append("Request: ").append(reqAndResp[0]).append("\nResponse: ").append(reqAndResp[1]).append("\n\n");
         }
-    }
 
-    private String loadValueFromFile() {
-        File file = new File(FILE_PATH);
-        if (file.exists()) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
-                return reader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
+        historyArea.setText(historyRequested.toString());
+
+        Button clearButton = new Button("Clear history");
+        clearButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white; -fx-alignment: bottom-right");
+        clearButton.setOnAction(event -> {
+            try {
+                clearHistory();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
+            stage.close();
+        });
+        Button submitButton = new Button("OK");
+        submitButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white; -fx-alignment: bottom-right");
+        submitButton.setOnAction(event -> {
+            stage.close();
+        });
+
+        HBox buttons = new HBox();
+//        buttons.setPadding(new Insets(10));
+        buttons.setSpacing(10);
+        buttons.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        buttons.getChildren().addAll(clearButton, submitButton);
+
+        VBox settings = new VBox();
+        settings.setPadding(new Insets(10));
+        settings.setSpacing(10);
+        settings.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        settings.getChildren().addAll(historyArea, buttons);
+        Scene scene = new Scene(settings, 400, 300);
+        stage.setScene(scene);
+        stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon.png")));
+        stage.showAndWait();
+    }
+
+    private void showSettingsDialog() {
+        // Создание диалогового окна с полями ввода
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.setTitle("Settings");
+
+        TextField tokenField = new TextField();
+        tokenField.setPromptText("Token");
+        tokenField.setText(userPreferences.get("token", ""));
+
+        Label tokenLabel = new Label("Enter your API token from ChatGPT");
+        tokenLabel.setLabelFor(tokenField);
+        tokenLabel.setStyle("-fx-text-fill: white;");
+
+        TextField historyMaxField = new TextField();
+        historyMaxField.setPromptText("Max history elements");
+        historyMaxField.setText(userPreferences.get("history", "10"));
+
+        Label historyLabel = new Label("Enter how many requests save to history (Default: 10)");
+        historyLabel.setLabelFor(historyMaxField);
+        historyLabel.setStyle("-fx-text-fill: white;");
+
+        CheckBox promptClear = new CheckBox("Clear prompt after sending request");
+        promptClear.setStyle("-fx-text-fill: white;");
+        if (userPreferences.getBoolean("clear", true)) {
+            promptClear.fire();
         }
-        return null;
+
+        Button submitButton = new Button("Submit");
+        submitButton.setStyle("-fx-background-color: #555555; -fx-text-fill: white; -fx-alignment: bottom-right");
+        submitButton.setOnAction(event -> {
+            TOKEN = tokenField.getText();
+            userPreferences.put("token", tokenField.getText());
+            CLEAR = promptClear.isSelected();
+            userPreferences.putBoolean("clear", promptClear.isSelected());
+            MAX_HISTORY = Integer.parseInt(historyMaxField.getText());
+            userPreferences.putInt("history", Integer.parseInt(historyMaxField.getText()));
+            stage.close();
+        });
+
+        VBox settings = new VBox();
+        settings.setPadding(new Insets(10));
+        settings.setSpacing(10);
+        settings.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        settings.getChildren().addAll(tokenLabel, tokenField, historyLabel, historyMaxField, promptClear, submitButton);
+        Scene scene = new Scene(settings, 300, 240);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon.png")));
+        stage.showAndWait();
+    }
+
+    public List<String[]> getAllHistory() throws SQLException {
+        List<String[]> historyList = new ArrayList<>();
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT rowid, request, response FROM history ORDER BY rowid DESC");
+        while (rs.next()) {
+            String[] historyItem = new String[]{rs.getString("request"), rs.getString("response")};
+            historyList.add(historyItem);
+        }
+        stmt.close();
+        return historyList;
+    }
+
+    public void addHistoryItem(String request, String response) throws SQLException {
+        int currentCount = getAllHistory().size();
+        if (currentCount >= MAX_HISTORY) {
+            PreparedStatement pstmt = connection.prepareStatement("DELETE FROM history WHERE rowid = (SELECT MIN(rowid) FROM history)");
+            pstmt.executeUpdate();
+            pstmt.close();
+        }
+        PreparedStatement pstmt = connection.prepareStatement("INSERT INTO history(request, response) VALUES(?, ?)");
+        pstmt.setString(1, request);
+        pstmt.setString(2, response);
+        pstmt.executeUpdate();
+        pstmt.close();
+    }
+
+    public void clearHistory() throws SQLException {
+        PreparedStatement pstmt = connection.prepareStatement("DELETE FROM history");
+        pstmt.executeUpdate();
+        pstmt.close();
     }
 }
